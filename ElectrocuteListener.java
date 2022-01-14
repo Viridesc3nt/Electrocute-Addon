4714750bac66a53fa1ace703e2fb5a8dafda7497
Viridesc3nt <97551484+Viridesc3nt@users.noreply.github.com>
2022-01-14 17:23:00 -0500

    Create ElectrocuteListener.java

---
+++ ElectrocuteListener.java
@@ -0,0 +1,29 @@
+package me.justinjaques.lightningball;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ElectrocuteListener implements Listener {
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+            Player player = event.getPlayer();
+            BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+
+            if (bPlayer.canBend(CoreAbility.getAbility(Electrocute.class))) {
+                new Electrocute(player);
+
+            }
+
+
+        }
+    }
+
+
+}
